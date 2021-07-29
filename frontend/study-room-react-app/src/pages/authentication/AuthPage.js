4af75b2e3822b2dd5f6e683b97a58b145cbdf7f9
amirali monjar <amirali.mnj@gmail.com>
2021-07-29 22:16:24 +0430

    Added states to auth

--- frontend/study-room-react-app/src/pages/authentication/AuthPage.js
+++ frontend/study-room-react-app/src/pages/authentication/AuthPage.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import SignUpForm from "../../components/forms/signup/SignUpForm";
 import LoginForm from "../../components/forms/login/LoginForm";
 import AuthOverlay from "../../components/overlays/auth/AuthOverlay";
-
+import axios from "axios";
 const AuthPage = (props) => {
   const [isSigningUp, setIsSigningUp] = useState(false);
   const openSignUp = () => {
@@ -12,13 +12,17 @@ const AuthPage = (props) => {
   const closeSignUp = () => {
     setIsSigningUp(false);
   };
+  const [loginData, setLoginData] = useState({});
+  const [signupData, setSignupData] = useState({});
+
+  const onSubmitSignUp = () => {};
   return (
     <div
       className={`auth-container ${isSigningUp ? "sign-up-active" : ""}`}
       id="container"
     >
-      <SignUpForm />
-      <LoginForm />
+      <SignUpForm setData={setSignupData} onSubmit={onSubmitSignUp} />
+      <LoginForm setData={setLoginData} />
       <AuthOverlay goRight={closeSignUp} goLeft={openSignUp} />
     </div>
   );
