b30cee40f8c9baa50be40a8064b230cab1487933
amirali monjar <amirali.mnj@gmail.com>
2021-07-30 11:43:20 +0430

    Added cors

--- backend/userservice/userservice/settings.py
+++ backend/userservice/userservice/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_jwt',
+    'corsheaders',
     'user'
 ]
 
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 PASSWORD_HASHERS = [
